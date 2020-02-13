f7f7f151fa2168fe6d5055b1c4767e7a71926a9a
Dalwyx <dalwyx@gmail.com>
2020-02-13 20:06:00 +0300

    Spawnable objects added

---
+++ Assets/Scripts/Game/SpawnableObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    [CreateAssetMenu(fileName = "New spawnable object", menuName = "Level/Spawnable Object")]
+    public class SpawnableObject : ScriptableObject
+    {
+        [SerializeField] private GameObject _prefab;
+        [SerializeField, Range(0, 1)] private float _rarety;
+
+        public GameObject Prefab => _prefab;
+        public float Rarety => _rarety;
+    } 
+}
