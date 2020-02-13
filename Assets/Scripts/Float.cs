c35dd3435fe9f90665ed6dd871977560b049bffd
Dalwyx <dalwyx@gmail.com>
2020-02-13 19:21:41 +0300

    Mover added

---
+++ Assets/Scripts/Float.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Float variable", menuName = "Var/Float")]
+public class Float : ScriptableObject
+{
+    [SerializeField] private float _value;
+    public float Value => _value;
+
+    public void SetValue(float value)
+    {
+        _value = value;
+    }
+}
---
+++ Assets/Scripts/Game/Mover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class Mover : MonoBehaviour
+    {
+        [SerializeField] private Float _baseSpeed;
+        [SerializeField] private float _speedModifier = 1;
+        [SerializeField] private Vector2 _moveDirection;
+
+        private void Update()
+        {
+            if (_baseSpeed == null)
+                return;
+
+            var translation = _baseSpeed.Value * _speedModifier * Time.deltaTime * _moveDirection;
+            transform.Translate(translation);
+        }
+    } 
+}
