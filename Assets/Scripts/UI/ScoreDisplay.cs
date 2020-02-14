599b265ce5c12d617d0950e52f7e4a0f98eaf682
Dalwyx <dalwyx@gmail.com>
2020-02-14 12:55:28 +0300

    Score display added

---
+++ Assets/Scripts/UI/ScoreDisplay.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Game;
+
+namespace UI
+{
+    [RequireComponent(typeof(Text))]
+    public class ScoreDisplay : MonoBehaviour
+    {
+        [SerializeField] private ScoreCounter _scoreCounter;
+        private Text _textField;
+
+        private void Awake()
+        {
+            _textField = GetComponent<Text>();
+            UpdateValue();
+        }
+
+        private void OnEnable()
+        {
+            if (_scoreCounter != null)
+                _scoreCounter.ScoreUpdated += UpdateValue;
+        }
+
+        private void OnDisable()
+        {
+            if (_scoreCounter != null)
+                _scoreCounter.ScoreUpdated -= UpdateValue;
+        }
+
+        private void UpdateValue()
+        {
+            if (_scoreCounter != null)
+                _textField.text = _scoreCounter.currentScore.ToString();
+        }
+    } 
+}
