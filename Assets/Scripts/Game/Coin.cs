de7e675ca4536ec2741a6b1ba1c8ace4468ae7aa
Dalwyx <dalwyx@gmail.com>
2020-02-14 12:36:38 +0300

    Coin collecting added

---
+++ Assets/Scripts/Game/Coin.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class Coin : MonoBehaviour
+    {
+        [SerializeField] private int coinValue = 1;
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            var counter = other.GetComponent<ScoreCounter>();
+            if (counter != null)
+            {
+                counter.AddScore(coinValue);
+                gameObject.SetActive(false);
+            }
+
+        }
+    } 
+}
---
+++ Assets/Scripts/Game/ScoreCounter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class ScoreCounter : MonoBehaviour
+    {
+        public int currentScore { get; private set; }
+        public event Action ScoreUpdated;
+
+        public void AddScore(int val)
+        {
+            currentScore += val;
+            ScoreUpdated?.Invoke();
+        }
+    } 
+}
