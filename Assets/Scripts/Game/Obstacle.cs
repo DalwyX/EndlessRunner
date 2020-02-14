5afb916afa6878f1a2f82893a524483547085c78
Dalwyx <dalwyx@gmail.com>
2020-02-14 15:06:50 +0300

    Obstacle script added

---
+++ Assets/Scripts/Game/Obstacle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game
+{
+    public class Obstacle : MonoBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.collider.GetComponent<Player>() != null)
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    } 
+}
