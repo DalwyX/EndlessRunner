ca43f7d06664a98006ce5a27c0db3f9b923a165f
Dalwyx <dalwyx@gmail.com>
2020-02-14 14:58:13 +0300

    Player component added

---
+++ Assets/Scripts/Game/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private Float _baseSpeed;
+        [SerializeField] private float _startingSpeed;
+        [SerializeField] private float _maxSpeed;
+        [SerializeField] private float _accelerationPerSecond;
+
+        private void Start()
+        {
+            _baseSpeed?.SetValue(_startingSpeed);
+        }
+
+        private void Update()
+        {
+            var accelerationThisFrame = _accelerationPerSecond * Time.deltaTime;
+            var speed = Mathf.Clamp(_baseSpeed.Value + accelerationThisFrame, _startingSpeed, _maxSpeed);
+            _baseSpeed?.SetValue(speed);
+        }
+    } 
+}
