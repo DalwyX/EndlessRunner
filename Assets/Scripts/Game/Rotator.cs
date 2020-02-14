3d55d390d85ce8614ad0f43dc85ad829b43df60d
Dalwyx <dalwyx@gmail.com>
2020-02-14 14:37:35 +0300

    Rotator added

---
+++ Assets/Scripts/Game/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField] private Vector3 _rotationAxis;
+        [SerializeField] private Float _baseSpeed;
+        [SerializeField] private float _modifier;
+
+        private void Awake()
+        {
+            _rotationAxis = _rotationAxis.normalized;
+        }
+
+        private void FixedUpdate()
+        {
+            transform.Rotate(_rotationAxis, _baseSpeed.Value * _modifier);
+        }
+    } 
+}
