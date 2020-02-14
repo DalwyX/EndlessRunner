a4cd1b2fc3f704a04ddea520a32520a72082e3eb
Dalwyx <dalwyx@gmail.com>
2020-02-14 14:01:14 +0300

    Menu script added

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class Menu : MonoBehaviour
+    {
+        [SerializeField] private string _modalOpenTrigger = "Open";
+        [SerializeField] private string _modalCloseTrigger = "Close";
+
+        public void LoadScene(int sceneIndex)
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+
+        public void CloseGame()
+        {
+            Application.Quit();
+        }
+
+        public void OpenModalWindow(Animator modalWindowAnimator)
+        {
+            modalWindowAnimator.SetTrigger(_modalOpenTrigger);
+        }
+
+        public void CloseModalWindow(Animator modalWindowAnimator)
+        {
+            modalWindowAnimator.SetTrigger(_modalCloseTrigger);
+        }
+    }
+}
