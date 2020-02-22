b16c7a901e54c3681e3a77ec6771917598fb63cd
Dalwyx <dalwyx@gmail.com>
2020-02-22 17:28:09 +0300

    Mover remade into Runner script

---
+++ Assets/Scripts/Movement/Runner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class Runner : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+        [SerializeField] private Vector2 _moveDirection;
+
+        private void OnValidate()
+        {
+            _speed = Mathf.Clamp(_speed, 0, Mathf.Infinity);
+            _moveDirection = _moveDirection.normalized;
+        }
+
+        private void Update()
+        {
+            var translation = _speed * Time.deltaTime * _moveDirection;
+            transform.Translate(translation);
+        }
+    } 
+}
