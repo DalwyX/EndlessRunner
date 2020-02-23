654482f20c9cea3a9a5daf5278f2307a5cfc7adb
Dalwyx <dalwyx@gmail.com>
2020-02-23 19:43:01 +0300

    Player animations added

--- Assets/Scripts/Game/Jumper.cs
+++ Assets/Scripts/Game/Jumper.cs
@@ -1,38 +1,76 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace Game
 {
     [RequireComponent(typeof(Rigidbody2D))]
     public class Jumper : MonoBehaviour
     {
+        private enum State
+        {
+            Grounded,
+            Jumping,
+            Falling
+        }
+
         [SerializeField] private float _jumpForce;
         [SerializeField] private float _fastJumpMuliplier = 2f;
         [SerializeField] private float _fallMultipier = 2.5f;
         [SerializeField] private LayerMask _groundLayer;
         [SerializeField] private KeyCode _jumpKey;
+        [SerializeField] private UnityEvent _jumped;
+        [SerializeField] private UnityEvent _grounded;
         private Rigidbody2D _rigidbody;
+        private State _state;
         private bool _isBlocked;
 
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+            _state = State.Grounded;
         }
 
         private void Update()
         {
-            if (Input.GetKeyDown(_jumpKey) && IsOnGround() && !_isBlocked)
+            switch (_state)
             {
-                _rigidbody.AddForce(_jumpForce * Vector2.up, ForceMode2D.Impulse);
+                case State.Grounded:
+                    Grounded();
+                    break;
+
+                case State.Jumping:
+                    Jumping();
+                    break;
+
+                case State.Falling:
+                    Falling();
+                    break;
             }
         }
 
-        private void FixedUpdate()
+        public void StopJumping()
+        {
+            _isBlocked = true;
+        }
+
+        private void Grounded()
+        {
+            if (Input.GetKeyDown(_jumpKey) && !_isBlocked)
+            {
+                _jumped?.Invoke();
+                _rigidbody.AddForce(_jumpForce * Vector2.up, ForceMode2D.Impulse);
+                _state = State.Jumping;
+            }
+        }
+
+        private void Jumping()
         {
             if (_rigidbody.velocity.y < 0)
             {
-                _rigidbody.gravityScale = _fallMultipier;
+                _state = State.Falling;
+                _rigidbody.gravityScale = 1;
             }
-            else if (_rigidbody.velocity.y > 0 && !Input.GetKey(_jumpKey))
+            else if (!Input.GetKey(_jumpKey))
             {
                 _rigidbody.gravityScale = _fastJumpMuliplier;
             }
@@ -42,9 +80,18 @@ namespace Game
             }
         }
 
-        public void StopJumping()
+        private void Falling()
         {
-            _isBlocked = true;
+            if (IsOnGround())
+            {
+                _rigidbody.gravityScale = 1;
+                _grounded?.Invoke();
+                _state = State.Grounded;
+            }
+            else
+            {
+                _rigidbody.gravityScale = _fallMultipier;
+            }
         }
 
         private bool IsOnGround()
